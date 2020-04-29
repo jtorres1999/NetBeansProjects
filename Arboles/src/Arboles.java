66d549489ed80cf064cdd457ec511e0f4cc8a2cf
jtorres1999 <tutorialesnetbeans@gmail.com>
2020-04-28 19:17:01 -0500

    Arboles

---
+++ Arboles/src/Arboles.java
@@ -0,0 +1,11 @@
+
+public class Arboles {
+
+    public static void main(String[] args) {
+    /*son estruturas de datos no lineales
+    compuestos por nodos finitos y lineeas dirigidas llamadas ramas
+    Un nodo no puede llamarse padre si tiene nodos sucesores  
+    Un arbol binario es una estructura recursiva, */
+    }
+    
+}
---
+++ Arboles/src/NodoArboles.java
@@ -0,0 +1,5 @@
+
+public class NodoArboles {
+    int Dato;
+    Nodo Arbol= new Nodo();
+}
