321f666dac72032e9eccc67a9c662f0b28686166
jtorres1999 <tutorialesnetbeans@gmail.com>
2020-05-01 16:57:19 -0500

    Conexion de java y mysql

---
+++ Conexion/src/Conexion.java
@@ -0,0 +1,24 @@
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+
+public class Conexion {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        conex cone=new conex();
+        Statement st;
+        ResultSet sr;
+        try {
+            st=cone.con.createStatement();
+            sr=st.executeQuery("select * from usuario");
+            while (sr.next()) {                
+                System.out.println(sr.getString("Nombre")+" "+sr.getInt("id")+" "+sr.getString("Profesion"));
+            }
+            cone.con.close();
+        } catch (Exception e) {
+        }
+    }
+    
+}
---
+++ Conexion/src/conex.java
@@ -0,0 +1,16 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class conex {
+    Connection con;
+    public conex() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql://localhost\\SQLEXPRESS/bd_Practica","jesus","Aa012345678");
+        } catch (Exception e) {
+            System.out.println("Error:" +e);
+        }
+}
+}
